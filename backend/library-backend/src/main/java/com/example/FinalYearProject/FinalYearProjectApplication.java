761e46a884cee5120df9915495b25d6140177f93
srinivas chowdary <kancharlasrinu11@gmail.com>
2025-12-19 18:12:48 +0530

    Added Spring Boot backend project

---
+++ backend/library-backend/src/main/java/com/example/FinalYearProject/FinalYearProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.FinalYearProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinalYearProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinalYearProjectApplication.class, args);
+	}
+
+}
---
+++ backend/library-backend/src/test/java/com/example/FinalYearProject/FinalYearProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.FinalYearProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinalYearProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
